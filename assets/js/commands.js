67c0f7558ff33b191ae2945a018ceb358bca999a
allegrojacc <allegrojacc@gmail.com>
2024-08-16 14:33:18 +0200

    rebuild

---
+++ assets/js/commands.js
@@ -0,0 +1,211 @@
+const TWITTER = "https://x.com/allegrojacc";
+const PSN = "https://psnprofiles.com/allegrojacc";
+const GITHUB = "https://github.com/allegrojacc";
+const EMAIL = "allegrojacc@gmail.com";
+const USERNAME = "allegrojacc";
+
+// const ABOUT = [
+  // "<br>",
+  // "Hello!👋",
+  // "I'm allegrojacc, a random gamer",
+  // "with a lifelong passion for",
+  // "games and technology! 🧑‍💻",
+  // "My journey with gaming began",
+  // "early in my childhood, sparking",
+  // "a fascination that led me to creating",
+  // "my own gaming projects.",
+  // "I thrive on learning new concepts",
+  // "and applying them to solve",
+  // "complex problems. ",
+  // "One of my proudest achievements is developing",
+  // "a fan-made Polish localization for Risk of Rain 2.",
+  // "Currently, I'm in the process of making my first game",
+  // "and I'm excited to bring my ideas to life! 🎮",
+  // "<br>",
+// ];
+
+const ABOUT = [
+  "<br>",
+  "Hello!👋",
+  "              ",
+  "I'm allegrojacc, a random gamer",
+  "with a lifelong passion for",
+  "games and technology! 🧑‍💻",
+  "                            ",
+  "My journey with gaming began",
+  "early in my childhood, sparking",
+  "a fascination that led me to creating",
+  "my own gaming projects.",
+  "                             ",
+  "I thrive on learning new concepts",
+  "and applying them to solve",
+  "complex problems. ",
+  "                             ",
+  "One of my proudest achievements is developing",
+  "a fan-made Polish localization for Risk of Rain 2.",
+  "Currently, I'm in the process of making my first game",
+  "and I'm excited to bring my ideas to life! 🎮",
+  "                                     ",
+  "<br>",
+];
+
+SOCIAL = [
+  "<br>",
+  'twitter        <a href="' +
+    TWITTER +
+    '" target="_blank">twitter/' +
+    USERNAME +
+    "</a>",
+  'psn            <a href="' +
+    PSN +
+    '" target="_blank">psn/' +
+    USERNAME +
+    "</a>",
+  'github         <a href="' +
+    GITHUB +
+    '" target="_blank">github/' +
+    USERNAME +
+    "</a>",
+  "<br>",
+];
+
+PROJECTS = [
+  "<br>",
+  "Here are my projects:",
+  "<br>",
+  "<strong> - Risk of Rain 2 - Polish localization project <strong>",
+  "The goal of this project is to create ",
+  "a complete Polish localization for Risk of Rain 2.",
+  "This includes translating the following into Polish:",
+  "<br>",
+  "user interface texts, item descriptions, character abilities, ",
+  "enemy details, level descriptions, and dialogues.",
+  "<a href=http://spolszczenie.allegrojacc.online/> GitHub link",
+  "<br>",
+  "<br>",
+  "<strong>Project Turalling",
+  "Game is in development at Pixelated Browar Games!",
+  "<a href=https://www.youtube.com/@Pixelated_Browar_Games/> Pixelated Browar Games>",
+  "<br>",
+];
+
+COMMAND_LIST = [
+  "<br>",
+  '<span class="command">about</span>          Learn more about me',
+  '<span class="command">social</span>         Display social networks',
+  '<span class="command">spage</span>          Page with all socials',
+  '<span class="command">projects</span>       View my projects',
+  '<span class="command">history</span>        Show command history',
+  '<span class="command">email</span>          Feel free to reach out',
+  '<span class="command">banner</span>         Display the header',
+  '<span class="command">clear</span>          Clear terminal',
+  '<span class="command">echo</span>           Print any give string',
+  '<span class="command">ls</span>             List directory content',
+  '<span class="command">terminal</span>       View OG creator of This Terminal',
+  '<span class="command">cd &lt;directory&gt</span> Change directory',
+  '<span class="command">help</span>           Display all commands',
+  "<br>",
+];
+
+TERMINAL = [
+  "<br>",
+  "Shout out to the original creator of this terminal",
+  "<a href=https://github.com/ealpizarp/portafolio-website> Eric's Github</a>"
+]
+
+BANNER = [
+
+
+"  ▄████████  ▄█        ▄█          ▄████████    ▄██████▄     ▄████████  ▄██████▄       ▄█    ▄████████  ▄████████  ▄████████       ▄██████▄     ▄████████ ",
+"  ███    ███ ███       ███         ███    ███   ███    ███   ███    ███ ███    ███     ███   ███    ███ ███    ███ ███    ███      ███    ███   ███    ███ ",
+"  ███    ███ ███       ███         ███    █▀    ███    █▀    ███    ███ ███    ███     ███   ███    ███ ███    █▀  ███    █▀       ███    ███   ███    █▀  ",
+"  ███    ███ ███       ███        ▄███▄▄▄      ▄███         ▄███▄▄▄▄██▀ ███    ███     ███   ███    ███ ███        ███             ███    ███   ███        ",
+"▀███████████ ███       ███       ▀▀███▀▀▀     ▀▀███ ████▄  ▀▀███▀▀▀▀▀   ███    ███     ███ ▀███████████ ███        ███             ███    ███ ▀███████████ ",
+"  ███    ███ ███       ███         ███    █▄    ███    ███ ▀███████████ ███    ███     ███   ███    ███ ███    █▄  ███    █▄       ███    ███          ███ ",
+"  ███    ███ ███▌    ▄ ███▌    ▄   ███    ███   ███    ███   ███    ███ ███    ███     ███   ███    ███ ███    ███ ███    ███      ███    ███    ▄█    ███ ",
+"  ███    █▀  █████▄▄██ █████▄▄██   ██████████   ████████▀    ███    ███  ▀██████▀  █▄ ▄███   ███    █▀  ████████▀  ████████▀        ▀██████▀   ▄████████▀  ",
+"             ▀         ▀                                     ███    ███            ▀▀▀▀▀▀                                                                  ",
+
+  
+];
+
+DIRECTORIES = ["<br>", "games","<br>"];
+
+EMAIL_INFO = [
+  "                 ",
+  "✉️ " + "<a href=mailto:" + EMAIL + ">" + EMAIL + "</a>",
+  "                  ",
+];
+
+SUDO = [
+  "                                                                ",
+  'Only allegrojacc has <span class="command">admin</span> privileges :(',
+  "                                                                ",
+];
+
+
+TERMINAL_INFO = [
+  "Welcome to allegrojacc os v1.0",
+  "For a list of available commands, type <span class=\"command\">'help'</span>.",
+];
+
+TERMINAL_INFO_MOBILE = [
+  "Welcome to allegrojacc os!",
+  "                                 ",
+  "For a list of available commands,",
+  "type <span class=\"command\">'help'</span>.",
+  "                                          ",
+  "Version (1.0)",
+  "                                          ",
+];
+
+// -----------------------SAMPLE ASCII ARTS --------------------
+
+
+// "  ▄████████  ▄█        ▄█          ▄████████    ▄██████▄     ▄████████  ▄██████▄       ▄█    ▄████████  ▄████████  ▄████████       ▄██████▄     ▄████████ ",
+// "  ███    ███ ███       ███         ███    ███   ███    ███   ███    ███ ███    ███     ███   ███    ███ ███    ███ ███    ███      ███    ███   ███    ███ ",
+// "  ███    ███ ███       ███         ███    █▀    ███    █▀    ███    ███ ███    ███     ███   ███    ███ ███    █▀  ███    █▀       ███    ███   ███    █▀  ",
+// "  ███    ███ ███       ███        ▄███▄▄▄      ▄███         ▄███▄▄▄▄██▀ ███    ███     ███   ███    ███ ███        ███             ███    ███   ███        ",
+// "▀███████████ ███       ███       ▀▀███▀▀▀     ▀▀███ ████▄  ▀▀███▀▀▀▀▀   ███    ███     ███ ▀███████████ ███        ███             ███    ███ ▀███████████ ",
+// "  ███    ███ ███       ███         ███    █▄    ███    ███ ▀███████████ ███    ███     ███   ███    ███ ███    █▄  ███    █▄       ███    ███          ███ ",
+// "  ███    ███ ███▌    ▄ ███▌    ▄   ███    ███   ███    ███   ███    ███ ███    ███     ███   ███    ███ ███    ███ ███    ███      ███    ███    ▄█    ███ ",
+// "  ███    █▀  █████▄▄██ █████▄▄██   ██████████   ████████▀    ███    ███  ▀██████▀  █▄ ▄███   ███    █▀  ████████▀  ████████▀        ▀██████▀   ▄████████▀  ",
+// "             ▀         ▀                                     ███    ███            ▀▀▀▀▀▀                                                                  ",
+
+
+MOBILE_BANNER = [
+
+// '       _ _                      _                            ',
+// '  __ _| | | ___  __ _ _ __ ___ (_) __ _  ___ ___    ___  ___ ',
+// ' / _` | | |/ _ \/ _` | '__/ _ \| |/ _` |/ __/ __|  / _ \/ __|',
+// '| (_| | | |  __/ (_| | | | (_) | | (_| | (_| (__  | (_) \__ \',
+// ' \__,_|_|_|\___|\__, |_|  \___// |\__,_|\___\___|  \___/|___/',
+// '                |___/        |__/    ',                        
+
+
+
+"       _ _                         _                                ",
+"      | | |                       (_)                               ",
+" _____| | | _____  ____  ____ ___  _ _____  ____ ____     ___   ___ ",
+"(____ | | || ___ |/ _  |/ ___) _ \| (____ |/ ___) ___)   / _ \ /___)",
+"/ ___ | | || ____( (_| | |  | |_| | / ___ ( (__( (___   | |_| |___ |",
+"\_____|\_)_)_____)\___ |_|   \___/| \_____|\____)____)   \___/(___/ ",
+"                 (_____|        (__/         "                       ,
+
+];
+
+// '       _ _                      _                            ',
+// '  __ _| | | ___  __ _ _ __ ___ (_) __ _  ___ ___    ___  ___ ',
+// ' / _` | | |/ _ \/ _` | '__/ _ \| |/ _` |/ __/ __|  / _ \/ __|',
+// '| (_| | | |  __/ (_| | | | (_) | | (_| | (_| (__  | (_) \__ \',
+// ' \__,_|_|_|\___|\__, |_|  \___// |\__,_|\___\___|  \___/|___/',
+// '                |___/        |__/    ',                        
+
+// '       _ _                      _                            ',
+// '  __ _| | | ___  __ _ _ __ ___ (_) __ _  ___ ___    ___  ___ ',
+// ' / _` | | |/ _ \/ _` | '__/ _ \| |/ _` |/ __/ __|  / _ \/ __|',
+// '| (_| | | |  __/ (_| | | | (_) | | (_| | (_| (__  | (_) \__ \',
+// ' \__,_|_|_|\___|\__, |_|  \___// |\__,_|\___\___|  \___/|___/',
+// '                |___/        |__/    ',                        
+
+
